78196703eb0514768555626ea56d02efbead5105
0rbianta <63746411+0rbianta@users.noreply.github.com>
2020-08-03 20:09:59 +0300

    UI update
    UI by 0rbianta.

--- app/src/main/java/krunal/com/example/rootcheck/MainActivity.java
+++ app/src/main/java/krunal/com/example/rootcheck/MainActivity.java
@@ -2,24 +2,42 @@ package krunal.com.example.rootcheck;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.scottyab.rootbeer.RootBeer;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView TextView;
+    private TextView txtResult;
+    private ImageView imgResult;
+    private Button btnChkRoot;
+    private CardView cvResult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView = findViewById(R.id.TextView);
+        txtResult=findViewById(R.id.txtResult);
+        imgResult=findViewById(R.id.imgResult);
+        btnChkRoot=findViewById(R.id.btnChkRoot);
+        cvResult=findViewById(R.id.cvRootResult);
 
-         RootBeer rootBeer = new RootBeer(this);
-         rootBeer.setLogging(true);
+    }
+
+    public void chkButtonClick(View v){
+        checkRoot();
+    }
+
+    private void checkRoot(){
+        btnChkRoot.setText("Check again");
+        RootBeer rootBeer = new RootBeer(this);
+        rootBeer.setLogging(true);
 //        if (rootBeer.isRooted()) {
 //            //we found indication of root
 //            Log.e("isRooted", String.valueOf(rootBeer.isRooted()));
@@ -33,14 +51,18 @@ public class MainActivity extends AppCompatActivity {
         if (rootBeer.isRootedWithoutBusyBoxCheck()){
             //we found indication of root
             Log.e("isRootedBusyBoxCheck", String.valueOf(rootBeer.isRooted()));
-            TextView.setText("Root Status:- Your device is Rooted.");
+            txtResult.setText("Your device is Rooted.");
+            cvResult.setVisibility(View.VISIBLE);
+            imgResult.setBackgroundResource(R.drawable.ic_baseline_lock_open_24);
+
 
         }else {
             //we didn't find indication of root
             Log.e("isRootedBusyBoxCheck", String.valueOf(rootBeer.isRooted()));
-            TextView.setText("Root Status:- Your device is Not Rooted.");
+            txtResult.setText("Your device is Not Rooted.");
+            cvResult.setVisibility(View.VISIBLE);
+            imgResult.setBackgroundResource(R.drawable.ic_baseline_lock_open_24);
+
         }
-
-
     }
 }
